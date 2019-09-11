0660153b56e70d379ef79f70ba8585f9bc63ea21
Markus Siering <markus277@gmail.com>
2019-09-11 13:29:19 +0200

    feat(options): add options, extract functions from functions
    because why not extract them?

--- README.md
+++ README.md
@@ -15,16 +15,25 @@ If you do not know how to run a node script, you should better wait until I've t
 1. Export an account statement from Mintos, including interest payments, deposits, affiliate payments
 1. Change the input file to whatever you have via the `csvFilePath` in `index.js`
 1. Run `node .` to let the script run
+1. Add [options](#options)
 1. Import the generated CSV file into Portfolio Performance
 1. ???
 1. Profit! :money_with_wings:
+
+## Options
+
+| Option        | Description                                                               | Default                                       |
+| ------------- | ------------------------------------------------------------------------- | --------------------------------------------- |
+| `--input`     | **Required value** Path to the input file (should be in the input folder) | `none`                                        |
+| `--delimiter` | The CSV file delimiter                                                    | `;`                                           |
+| `--output`    | File output path                                                          | `output/import-for-portfolio-performance.csv` |
 
 ## Roadmap
 
 Hahaha, "roadmap". Well, whatever. Things that should happen:
 
-- Make input file a config
-- Parse any possible account statement content instead of just a few
-- Make a website where you drop the file & receive some CSV output for that file
-- More documentation!
-- Tests (hahaha! :wink:)
+- [x] Make input file a config
+- [ ] Parse any possible account statement content instead of just a few
+- [ ] Make a website where you drop the file & receive some CSV output for that file
+- [x] More documentation!
+- [ ] Tests (hahaha! :wink:)
--- index.js
+++ index.js
@@ -2,6 +2,34 @@ const fs = require('fs')
 const path = require('path')
 const csv = require('csvtojson')
 
+const getOptions = argv => {
+  const options = argv
+    .splice(2)
+    .reduce((acc, el, i, src) => {
+      const next = src[i + 1]
+      if (/^--/.test(el) && next && !/^--/.test(next)) {
+        el = el + '=' + next
+        acc.push(el)
+        return acc
+      }
+
+      if (/^--/.test(el)) {
+        acc.push(el)
+      }
+
+      return acc
+    }, [])
+    .reduce((acc, el) => {
+      const cleaned = el.replace(/^--/, '').split('=')
+      acc[cleaned[0]] = cleaned[1] || true
+      return acc
+    }, {})
+
+  return options
+}
+
+const options = getOptions(process.argv)
+
 const TRANSACTION_ID = 'Transaction ID'
 const TRANSACTION_DATE = 'Date'
 const TRANSACTION_DETAILS = 'Details'
@@ -11,14 +39,20 @@ const TYPE_INTEREST = 'Zinsen'
 const TYPE_DEPOSIT = 'Einlage'
 const TYPE_WITHDRAWAL = 'Entnahme'
 
+const DELIMITER = options.delimiter || ','
+const INPUT_PATH = options.input
+const OUTPUT_PATH =
+  options.output || 'output/import-for-portfolio-performance.csv'
+
 const CSV_HEADER =
   'Datum;Typ;Wert;Buchungswährung;Steuern;Stück;ISIN;WKN;Ticker-Symbol;Wertpapiername;Notiz\n'
 
-const csvFilePath = path.join(__dirname, 'input/20190902-account-statement.csv')
-const csvOutputPath = path.join(
-  __dirname,
-  'output/import-for-portfolio-performance.csv'
-)
+if (!options.input) {
+  throw new Error('No input file – no output. Stopping the process.')
+}
+
+const csvFilePath = path.join(__dirname, INPUT_PATH)
+const csvOutputPath = path.join(__dirname, OUTPUT_PATH)
 
 const parseableNumber = number => number.replace(',', '.')
 const parseableDate = date => date.replace(' ', 'T')
@@ -32,51 +66,67 @@ const isCreditInvest = string => {
   )
 }
 
-const getTransactionType = string => {
-  if (
+const isDeposit = string => {
+  return (
     string.includes('Incoming client payment') ||
     string.includes('Affiliate bonus')
-  ) {
-    return TYPE_DEPOSIT
-  }
-  if (
+  )
+}
+
+const isInterest = string => {
+  return (
     string.includes('Interest income') ||
     string.includes('Late payment fee income') ||
     string.includes('Delayed interest income')
-  ) {
+  )
+}
+
+const isWithdrawal = string => {
+  return string.includes('Reversed incoming client payment')
+}
+
+const getTransactionType = string => {
+  if (isDeposit(string)) {
+    return TYPE_DEPOSIT
+  }
+  if (isInterest(string)) {
     return TYPE_INTEREST
   }
-  if (string.includes('Reversed incoming client payment')) {
+  if (isWithdrawal(string)) {
     return TYPE_WITHDRAWAL
   }
   console.log(`######## Could not process: ${string}`)
   return ''
 }
 
+const createCsvRow = datum => {
+  const interest = parseableNumber(datum[TRANSACTION_TURNOVER])
+  const noteString = `ID: ${datum[TRANSACTION_ID]}, ${datum[TRANSACTION_DETAILS]}`
+  const entry = {
+    amount: numberToCommaSeparatedString(interest),
+    date: parseableDate(datum[TRANSACTION_DATE]),
+    type: getTransactionType(datum[TRANSACTION_DETAILS]),
+    currency: datum[TRANSACTION_CURRENCY],
+    note: noteString
+  }
+  return `${entry.date};${entry.type};${entry.amount};${entry.currency};;;;;;;${entry.note}\n`
+}
+
 async function transformCsvExport(path) {
   const data = await csv({
-    delimiter: ';'
+    delimiter: DELIMITER
   }).fromFile(path)
   let csvData = CSV_HEADER
   data
     .filter(datum => !isCreditInvest(datum[TRANSACTION_DETAILS]))
     .forEach(datum => {
-      const interest = parseableNumber(datum[TRANSACTION_TURNOVER])
-      const noteString = `ID: ${datum[TRANSACTION_ID]}, ${datum[TRANSACTION_DETAILS]}`
-      const entry = {
-        amount: numberToCommaSeparatedString(interest),
-        date: parseableDate(datum[TRANSACTION_DATE]),
-        type: getTransactionType(datum[TRANSACTION_DETAILS]),
-        currency: datum[TRANSACTION_CURRENCY],
-        note: noteString
-      }
-      csvData += `${entry.date};${entry.type};${entry.amount};${entry.currency};;;;;;;${entry.note}\n`
+      csvData += createCsvRow(datum)
     })
   fs.writeFile(csvOutputPath, csvData, function(err) {
     if (err) {
       return console.log(err)
     }
-    console.log('The file was saved to', csvOutputPath)
+    console.log('🚀 The file was saved to', csvOutputPath)
   })
 }
 
